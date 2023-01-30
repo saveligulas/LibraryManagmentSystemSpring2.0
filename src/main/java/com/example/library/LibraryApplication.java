48b29f42f644b0f621df1eaf32734622c8a7af19
saveligulas <saveli.gulas@gmail.com>
2023-01-30 13:45:40 +0100

    Removed methods from LibraryApplication.java

--- src/main/java/com/example/library/LibraryApplication.java
+++ src/main/java/com/example/library/LibraryApplication.java
@@ -1,24 +1,13 @@
 package com.example.library;
 
-import com.example.library.user.Customer;
-import org.joda.time.DateTime;
-import org.joda.time.LocalDate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class LibraryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public DateTime hello() {
-		return DateTime.now();
 	}
 
 }
