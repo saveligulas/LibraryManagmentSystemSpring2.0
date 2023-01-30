f98ca4491226ab5291913e8ed6689fbad164c748
saveligulas <saveli.gulas@gmail.com>
2023-01-30 13:45:57 +0100

    New CustomerController.java

---
+++ src/main/java/com/example/library/user/CustomerController.java
@@ -0,0 +1,26 @@
+package com.example.library.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/customer")
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerService.getCustomers();
+    }
+}
