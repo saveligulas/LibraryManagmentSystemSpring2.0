8594cf2e27353baad3224df2f3602eeb2660cb76
saveligulas <saveli.gulas@gmail.com>
2023-02-01 16:35:09 +0100

    Testing in LibraryApplication.java

--- src/main/java/com/example/library/LibraryApplication.java
+++ src/main/java/com/example/library/LibraryApplication.java
@@ -1,11 +1,17 @@
 package com.example.library;
 
 import com.example.library.user.Customer;
+import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManagerFactory;
+import java.sql.SQLException;
 import java.time.LocalDate;
 
 @SpringBootApplication
@@ -19,11 +25,10 @@ public class LibraryApplication {
 	@GetMapping("/")
 	public String hello() {
 		return new Customer(
-				"Saveli",
-				"saveli.gulas@gmail.com",
+				"Test",
+				"test@gmail.com",
 				LocalDate.of(2004,11,18),
 				18
 		).toString();
 	}
-
 }
