c693181dc077c270952ae882da396499f10d6f1f
saveligulas <saveli.gulas@gmail.com>
2023-02-02 11:42:08 +0100

    New CustomerConfig.java

---
+++ src/main/java/com/example/library/user/CustomerConfig.java
@@ -0,0 +1,36 @@
+package com.example.library.user;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class CustomerConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
+        return args -> {
+            Customer saveli = new Customer(
+                    "Saveli",
+                    "saveli.gulas@gmail.com",
+                    LocalDate.of(2004,11,18),
+                    18
+            );
+
+            Customer alex = new Customer(
+                    "Alex",
+                    "alex@dummy.com",
+                    LocalDate.of(2000, Month.DECEMBER,24),
+                    22
+            );
+
+            customerRepository.saveAll(
+                    List.of(saveli,alex)
+            );
+        };
+    }
+}
