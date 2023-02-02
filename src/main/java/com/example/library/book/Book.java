466b13023773c1cdcbdf5abe360ea3007c7095c4
saveligulas <saveli.gulas@gmail.com>
2023-02-02 11:19:54 +0100

    Added Book body

--- src/main/java/com/example/library/book/Book.java
+++ src/main/java/com/example/library/book/Book.java
@@ -1,4 +1,82 @@
 package com.example.library.book;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private Long id;
+    private String name;
+    private String author;
+    private Integer year;
+    private Boolean available;
+
+    public Book() {
+
+    }
+
+    public Book(Long id, String name, String author, Integer year, Boolean available) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.year = year;
+        this.available = available;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                ", available=" + available +
+                '}';
+    }
 }
