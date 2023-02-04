83c40886d6c873369057417f8336a3199c55d226
saveligulas <saveli.gulas@gmail.com>
2023-02-04 16:43:32 +0100

    changed CustomerRepository.findCustomerByEmail

--- src/main/java/com/example/library/user/CustomerRepository.java
+++ src/main/java/com/example/library/user/CustomerRepository.java
@@ -1,7 +1,11 @@
 package com.example.library.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
-
+    @Query("SELECT s FROM Customer s WHERE s.email = ?1")
+    Optional<Customer> findCustomerByEmail(String email);
 }
