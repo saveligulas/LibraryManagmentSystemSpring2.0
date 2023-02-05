5cff61dcbb7f06145323d75b4363df44a3934de6
saveligulas <saveli.gulas@gmail.com>
2023-02-05 21:15:56 +0100

    Cleared CustomerConfig

--- src/main/java/com/example/library/user/CustomerConfig.java
+++ src/main/java/com/example/library/user/CustomerConfig.java
@@ -14,21 +14,6 @@ public class CustomerConfig {
     @Bean
     CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
         return args -> {
-            Customer saveli = new Customer(
-                    "Saveli",
-                    "saveli.gulas@gmail.com",
-                    LocalDate.of(2004,11,18)
-            );
-
-            Customer alex = new Customer(
-                    "Alex",
-                    "alex@dummy.com",
-                    LocalDate.of(2000, Month.DECEMBER,24)
-            );
-
-            customerRepository.saveAll(
-                    List.of(saveli,alex)
-            );
         };
     }
 }
