3448483176d5f71f7d758800c9615d885cb12b55
saveligulas <saveli.gulas@gmail.com>
2023-02-06 10:40:54 +0100

    updates to BookService

---
+++ src/main/java/com/example/library/book/BookService.java
@@ -0,0 +1,45 @@
+package com.example.library.book;
+
+import com.example.library.user.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getBooks() {
+        return bookRepository.findAll();
+    }
+
+    public void addNewBook(Book book) {
+        Optional<Book> bookByName = bookRepository.findBookByName(book.getName());
+        if(bookByName.isPresent()) {
+            throw new IllegalStateException("book name taken");
+        }
+        bookRepository.save(book);
+    }
+
+    public void deleteBook(Long bookId) {
+        boolean exists = bookRepository.existsById(bookId);
+        if(!exists) {
+            throw new IllegalStateException("book with id "+ bookId + " does not exist");
+        }
+        bookRepository.deleteById(bookId);
+        bookRepository.flush();
+    }
+
+    public void deleteAllBooks() {
+        bookRepository.deleteAll();
+        bookRepository.flush();
+    }
+}
