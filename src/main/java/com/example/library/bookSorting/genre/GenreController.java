2da05b765f6feef4223ab9a6166ee3eda3a396d0
saveligulas <saveli.gulas@gmail.com>
2023-02-07 22:54:14 +0100

    new Genre api classes

---
+++ src/main/java/com/example/library/bookSorting/genre/GenreController.java
@@ -0,0 +1,9 @@
+package com.example.library.bookSorting.genre;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/genre")
+public class GenreController {
+}
---
+++ src/main/java/com/example/library/bookSorting/genre/GenreService.java
@@ -0,0 +1,4 @@
+package com.example.library.bookSorting.genre;
+
+public class GenreService {
+}
