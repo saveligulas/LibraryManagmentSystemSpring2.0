7128fded749fa8dc9f6f926a52ebca6b660ea1d0
saveligulas <saveli.gulas@gmail.com>
2023-02-07 23:21:55 +0100

    setup GenreConfig

--- src/main/java/com/example/library/bookSorting/genre/GenreConfig.java
+++ src/main/java/com/example/library/bookSorting/genre/GenreConfig.java
@@ -1,4 +1,15 @@
 package com.example.library.bookSorting.genre;
 
+import com.example.library.user.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class GenreConfig {
+    @Bean
+    CommandLineRunner genreCommandLineRunner(GenreRepository genreRepository) {
+        return args -> {
+        };
+    }
 }
