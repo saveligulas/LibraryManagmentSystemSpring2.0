d8b18c1661658d068372c356a7bd1a11dfe738e3
saveligulas <saveli.gulas@gmail.com>
2023-02-07 23:22:45 +0100

    added GenreRepository.findByName

--- src/main/java/com/example/library/bookSorting/genre/GenreRepository.java
+++ src/main/java/com/example/library/bookSorting/genre/GenreRepository.java
@@ -1,6 +1,11 @@
 package com.example.library.bookSorting.genre;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface GenreRepository extends JpaRepository<Genre, Long> {
+    @Query("SELECT g FROM Genre g WHERE g.name = ?1")
+    Optional<Genre> findByName(String name);
 }
