f68586e3739bba99384eb416a4ed6ab508f2ffa7
saveligulas <saveli.gulas@gmail.com>
2023-02-07 22:53:16 +0100

    new Author

--- src/main/java/com/example/library/bookSorting/author/Author.java
+++ src/main/java/com/example/library/bookSorting/author/Author.java
@@ -1,6 +1,9 @@
 package com.example.library.bookSorting.author;
 
+import com.example.library.bookSorting.book.Book;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "author")
@@ -9,8 +12,29 @@ public class Author {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
+    private String name;
+    @ManyToMany
+    @JoinTable(
+            name="authors_books",
+            joinColumns = @JoinColumn(name = "AUTHOR_ID"),
+            inverseJoinColumns = @JoinColumn(name = "BOOK_ID")
+    )
+    private List<Book> books;
 
+    public Author() {
 
+    }
+
+    public Author(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Author(Long id, String name, List<Book> books) {
+        this.id = id;
+        this.name = name;
+        this.books = books;
+    }
 
     public Long getId() {
         return id;
@@ -20,4 +44,19 @@ public class Author {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
 }
