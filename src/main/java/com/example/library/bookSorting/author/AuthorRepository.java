dc08d6109c4a3c37271ea0bca581886f07c4e346
saveligulas <saveli.gulas@gmail.com>
2023-02-08 10:51:32 +0100

    added AuthorRepository.findByListContains

--- src/main/java/com/example/library/bookSorting/author/AuthorRepository.java
+++ src/main/java/com/example/library/bookSorting/author/AuthorRepository.java
@@ -1,6 +1,14 @@
 package com.example.library.bookSorting.author;
 
+import com.example.library.bookSorting.book.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    @Query("SELECT a FROM Author a WHERE :book MEMBER OF e.books")
+    Optional<Author> findByListContains(@Param("book") Book book);
 }
