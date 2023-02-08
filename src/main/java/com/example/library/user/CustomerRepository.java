ab73ce8da9b708d5a9c5c7b4efc5bc740ec15d97
saveligulas <saveli.gulas@gmail.com>
2023-02-08 10:45:44 +0100

    added CustomerRepository.findByListContainsBook

--- src/main/java/com/example/library/user/CustomerRepository.java
+++ src/main/java/com/example/library/user/CustomerRepository.java
@@ -1,7 +1,9 @@
 package com.example.library.user;
 
+import com.example.library.bookSorting.book.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +14,7 @@ public interface CustomerRepository extends JpaRepository<Customer,Long> {
 
     @Query("SELECT s FROM Customer s WHERE s.name = ?1")
     Optional<Customer> findCustomerByName(String name);
+
+    @Query("SELECT s FROM Customer s WHERE :book MEMBER OF s.books")
+    Optional<Customer> findByListContainsBook(@Param("book") Book book);
 }
