513d3eb5daaf054af30d483b2a239deebfe6452e
saveligulas <saveli.gulas@gmail.com>
2023-02-09 11:38:07 +0100

    added AuthorRepository.findAuthorByName

--- src/main/java/com/example/library/bookSorting/author/AuthorRepository.java
+++ src/main/java/com/example/library/bookSorting/author/AuthorRepository.java
@@ -1,6 +1,7 @@
 package com.example.library.bookSorting.author;
 
 import com.example.library.bookSorting.book.Book;
+import com.example.library.user.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,6 +9,9 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    @Query("SELECT a FROM Author a WHERE a.name = ?1")
+    Optional<Author> findAuthorByName(String name);
 
     @Query("SELECT a FROM Author a WHERE :book MEMBER OF a.books")
     Optional<Author> findByListContains(@Param("book") Book book);
