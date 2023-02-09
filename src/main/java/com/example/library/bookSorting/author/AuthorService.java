fa1da2f851155d86cb13944a6578aa6b984eb404
saveligulas <saveli.gulas@gmail.com>
2023-02-09 11:17:55 +0100

    added AuthorService.deleteBook and setup AuthorService

--- src/main/java/com/example/library/bookSorting/author/AuthorService.java
+++ src/main/java/com/example/library/bookSorting/author/AuthorService.java
@@ -1,4 +1,31 @@
 package com.example.library.bookSorting.author;
 
+import com.example.library.bookSorting.book.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
 public class AuthorService {
+
+    AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @Transactional
+    public void deleteBook(Book book) {
+        Optional<Author> authorOptional = authorRepository.findByListContains(book);
+        if(authorOptional.isPresent()) {
+            Author author = authorRepository.findByListContains(book)
+                    .orElseThrow(() -> new IllegalStateException(
+                            "no author with book " + book.getName() + " exists"));
+            author.deleteBook(book);
+        }
+    }
+
 }
