9911969580ce2960c797923d3d3fad899102d754
saveligulas <saveli.gulas@gmail.com>
2023-02-09 11:16:33 +0100

    setup AuthorConfig.authorCommandLineRunner

--- src/main/java/com/example/library/bookSorting/author/AuthorConfig.java
+++ src/main/java/com/example/library/bookSorting/author/AuthorConfig.java
@@ -1,4 +1,16 @@
 package com.example.library.bookSorting.author;
 
+import com.example.library.bookSorting.book.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AuthorConfig {
+
+    @Bean
+    CommandLineRunner authorCommandLineRunner(AuthorRepository authorRepository) {
+        return args -> {
+        };
+    }
 }
