73f6ae2fc5471603ad29117e65b7d432d6bc21ff
saveligulas <saveli.gulas@gmail.com>
2023-02-09 11:16:45 +0100

    setup AuthorController

--- src/main/java/com/example/library/bookSorting/author/AuthorController.java
+++ src/main/java/com/example/library/bookSorting/author/AuthorController.java
@@ -1,9 +1,20 @@
 package com.example.library.bookSorting.author;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "api/v1/author")
 public class AuthorController {
+
+    private final AuthorService authorService;
+
+    private final AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorController(AuthorService authorService, AuthorRepository authorRepository) {
+        this.authorService = authorService;
+        this.authorRepository = authorRepository;
+    }
 }
